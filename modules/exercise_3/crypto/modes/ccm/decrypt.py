2d571b85fcc1ec1f9f277ab9f3d40d2e508d33d9
maximuspileus <krepekamadej@gmail.com>
2023-11-16 19:17:49 +0100

    fixed ccm

--- modules/exercise_3/crypto/modes/ccm/decrypt.py
+++ modules/exercise_3/crypto/modes/ccm/decrypt.py
@@ -2,33 +2,29 @@ import pyaes
 
 
 def aes_ccm_decrypt(ciphertext, key, nonce):
+    # Create an AES cipher object with the provided key
     aes = pyaes.AES(key)
+
+    # Set the block size (standard for AES)
     block_size = 16
+
+    # Calculate the number of blocks needed
     m = len(ciphertext)
     q = (m - 1) // block_size + 1
+
+    # Divide the ciphertext into blocks
     blocks = [ciphertext[i:i + block_size] for i in range(0, m, block_size)]
 
-    # Counter Mode
+    # Counter Mode (CTR) Decryption
     counter_bytes = b'\x00\x00\x00\x01'
     counter_64_bit = counter_bytes + b'\x00\x00\x00\x00'
     counter_block = nonce + counter_64_bit
 
-    # Counter Mode encryption
-    encrypted_blocks = [aes.encrypt(counter_block) for _ in range(q)]
+    # Decrypt each block separately
+    decrypted_blocks = [bytes(x ^ y for x, y in zip(aes.encrypt(counter_block), block)) for block in blocks]
 
-    # Calculate CBC-MAC
-    mac = encrypted_blocks[-1]  # The last block is the MAC
-
-    # Decrypt the ciphertext using Counter Mode
-    decrypted_blocks = [bytes(x ^ y for x, y in zip(e_block, block)) for e_block, block in zip(encrypted_blocks[:-1], blocks)]
-
-    # Combine decrypted blocks to get plaintext
+    # Combine decrypted blocks to get the plaintext
     plaintext = b''.join(decrypted_blocks)
-
-    # Verify MAC (optional but recommended for security)
-    calculated_mac = aes.encrypt(nonce + counter_bytes)  # Calculate MAC based on nonce and counter
-    if calculated_mac != mac:
-        raise ValueError("MAC verification failed. The message may have been tampered with.")
 
     return plaintext
 
--- modules/exercise_3/crypto/modes/ccm/encrypt.py
+++ modules/exercise_3/crypto/modes/ccm/encrypt.py
@@ -5,25 +5,26 @@ def aes_ccm_encrypt(data, key, nonce):
     aes = pyaes.AES(key)
     block_size = 16
     m = len(data)
+
+    # Calculate the number of blocks needed
     q = (m - 1) // block_size + 1
+
+    # Divide the data into blocks
     blocks = [data[i:i + block_size] for i in range(0, m, block_size)]
 
-    # Counter Mode
+    # Counter Mode (CTR) Encryption
     counter_bytes = b'\x00\x00\x00\x01'
     counter_64_bit = counter_bytes + b'\x00\x00\x00\x00'
     counter_block = nonce + counter_64_bit
 
-    # Counter Mode Encryption
+    # Encrypt each block separately
     encrypted_blocks = [aes.encrypt(counter_block) for _ in range(q)]
 
     # XOR each encrypted block with the corresponding plaintext block
-    ciphertext_blocks = [bytes(x ^ y for x, y in zip(e_block, block)) for e_block, block in zip(encrypted_blocks, blocks)]
+    ciphertext_blocks = [bytes(x ^ y for x, y in zip(e_block, block)) for e_block, block in
+                         zip(encrypted_blocks, blocks)]
 
     # Combine encrypted blocks to get the ciphertext
     ciphertext = b''.join(ciphertext_blocks)
-
-    # Calculate CBC-MAC (optional but recommended for security)
-    mac = aes.encrypt(nonce + counter_bytes)  # Calculate MAC based on nonce and counter
-    ciphertext += mac  # Append the MAC to the ciphertext
 
     return ciphertext

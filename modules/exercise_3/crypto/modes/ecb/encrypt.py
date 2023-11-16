016e79a4935c4c758f10880a3e3e549a76cd980a
maximuspileus <krepekamadej@gmail.com>
2023-11-16 18:20:03 +0100

    updated ecb

--- modules/exercise_3/crypto/modes/ecb/encrypt.py
+++ modules/exercise_3/crypto/modes/ecb/encrypt.py
@@ -1,12 +1,14 @@
 import pyaes
 
+from modules.exercise_3.crypto.utils.utils import pad_data
+
 
 def aes_ecb_encrypt(data, key):
     aes = pyaes.AES(key)
     block_size = 16
-    m = len(data)
-    q = (m - 1) // block_size + 1
-    blocks = [data[i:i + block_size] for i in range(0, m, block_size)]
+
+    padded_data = pad_data(data, block_size)
+    blocks = [padded_data[i:i + block_size] for i in range(0, len(padded_data), block_size)]
 
     # Electronic Codebook
     ciphertext = b''

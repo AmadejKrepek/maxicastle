ae12712e4a9e7749fbb1ac8fca99391fbd300bca
maximuspileus <krepekamadej@gmail.com>
2023-11-16 18:39:24 +0100

    added more user friednly code for ccm

--- modules/exercise_3/crypto/modes/ccm/decrypt.py
+++ modules/exercise_3/crypto/modes/ccm/decrypt.py
@@ -5,17 +5,30 @@ def aes_ccm_decrypt(ciphertext, key, nonce):
     aes = pyaes.AES(key)
     block_size = 16
     m = len(ciphertext)
-    q = (m - 1) // 16 + 1
-    blocks = [ciphertext[i:i+block_size] for i in range(0, m, block_size)]
+    q = (m - 1) // block_size + 1
+    blocks = [ciphertext[i:i + block_size] for i in range(0, m, block_size)]
 
-    # Counter mode
+    # Counter Mode
     counter_bytes = b'\x00\x00\x00\x01'
     counter_64_bit = counter_bytes + b'\x00\x00\x00\x00'
     counter_block = nonce + counter_64_bit
+
+    # Counter Mode encryption
     encrypted_blocks = [aes.encrypt(counter_block) for _ in range(q)]
 
-    decrypted_blocks = [bytes(x ^ y for x, y in zip(e_block, block)) for e_block, block in zip(encrypted_blocks, blocks)]
+    # Calculate CBC-MAC
+    mac = encrypted_blocks[-1]  # The last block is the MAC
+
+    # Decrypt the ciphertext using Counter Mode
+    decrypted_blocks = [bytes(x ^ y for x, y in zip(e_block, block)) for e_block, block in zip(encrypted_blocks[:-1], blocks)]
+
+    # Combine decrypted blocks to get plaintext
     plaintext = b''.join(decrypted_blocks)
+
+    # Verify MAC (optional but recommended for security)
+    calculated_mac = aes.encrypt(nonce + counter_bytes)  # Calculate MAC based on nonce and counter
+    if calculated_mac != mac:
+        raise ValueError("MAC verification failed. The message may have been tampered with.")
 
     return plaintext
 
--- modules/exercise_3/crypto/modes/ccm/encrypt.py
+++ modules/exercise_3/crypto/modes/ccm/encrypt.py
@@ -5,14 +5,25 @@ def aes_ccm_encrypt(data, key, nonce):
     aes = pyaes.AES(key)
     block_size = 16
     m = len(data)
-    q = (m - 1) // 16 + 1
+    q = (m - 1) // block_size + 1
     blocks = [data[i:i + block_size] for i in range(0, m, block_size)]
 
+    # Counter Mode
     counter_bytes = b'\x00\x00\x00\x01'
-    # Pad the counter to make it 64 bits (8 bytes)
     counter_64_bit = counter_bytes + b'\x00\x00\x00\x00'
     counter_block = nonce + counter_64_bit
+
+    # Counter Mode Encryption
     encrypted_blocks = [aes.encrypt(counter_block) for _ in range(q)]
-    ciphertext = b''.join(bytes(x ^ y for x, y in zip(encrypted_blocks[i], blocks[i])) for i in range(q))
+
+    # XOR each encrypted block with the corresponding plaintext block
+    ciphertext_blocks = [bytes(x ^ y for x, y in zip(e_block, block)) for e_block, block in zip(encrypted_blocks, blocks)]
+
+    # Combine encrypted blocks to get the ciphertext
+    ciphertext = b''.join(ciphertext_blocks)
+
+    # Calculate CBC-MAC (optional but recommended for security)
+    mac = aes.encrypt(nonce + counter_bytes)  # Calculate MAC based on nonce and counter
+    ciphertext += mac  # Append the MAC to the ciphertext
 
     return ciphertext

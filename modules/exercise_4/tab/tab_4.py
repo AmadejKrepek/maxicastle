ee675854345f50a54cac65aaecfd196fd619a2dd
maximuspileus <krepekamadej@gmail.com>
2023-11-27 18:14:51 +0100

    added xor

--- modules/exercise_4/tab/tab_4.py
+++ modules/exercise_4/tab/tab_4.py
@@ -5,23 +5,54 @@ import hmac
 import os
 import binascii
 
+
 def generate_salt(length=16):
     return os.urandom(length)
+
 
 def sha256_hash(password, salt):
     hashed_password = hashlib.sha256(salt + password.encode('utf-8')).hexdigest()
     return hashed_password
 
+
 def sha512_hash(password, salt):
     hashed_password = hashlib.sha512(salt + password.encode('utf-8')).hexdigest()
     return hashed_password
 
+
+def xor_bytes(b1, b2):
+    return bytes(x ^ y for x, y in zip(b1, b2))
+
+
 def generate_hmac(key, message):
-    return hmac.new(key, message.encode('utf-8'), hashlib.sha256).hexdigest()
+    block_size = 64  # Block size for SHA-256
+    opad = bytes(x ^ 0x5c for x in range(256))
+    ipad = bytes(x ^ 0x36 for x in range(256))
+
+    # Ensure the key is less than the block size
+    if len(key) > block_size:
+        key = hashlib.sha256(key).digest()
+
+    # Pad the key if it is less than the block size
+    key = key.ljust(block_size, b'\x00')
+
+    # XOR the key with ipad and opad
+    key_ipad = xor_bytes(key, ipad)
+    key_opad = xor_bytes(key, opad)
+
+    # Calculate inner hash
+    inner_hash = hashlib.sha256(key_ipad + message.encode('utf-8')).digest()
+
+    # Calculate outer hash
+    outer_hash = hashlib.sha256(key_opad + inner_hash).hexdigest()
+
+    return outer_hash
+
 
 def pbkdf2(password, salt, iterations=10000, key_length=32):
     derived_key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, iterations, key_length)
     return binascii.hexlify(derived_key).decode('utf-8')
+
 
 def create_tab4_controls(tab4):
     # Functions for UI controls on Tab 4
@@ -30,7 +61,6 @@ def create_tab4_controls(tab4):
         salt = generate_salt()
 
         sha256_result.set(sha256_hash(password, salt))
-        sha512_result.set(sha512_hash(password, salt))
 
     def calculate_hmac():
         key = hmac_key_entry.get()
@@ -51,10 +81,6 @@ def create_tab4_controls(tab4):
     ttk.Label(tab4, text="SHA-256 Result:").grid(column=0, row=1, padx=10, pady=10)
     sha256_result = tk.StringVar()
     ttk.Label(tab4, textvariable=sha256_result).grid(column=1, row=1, padx=10, pady=10)
-
-    ttk.Label(tab4, text="SHA-512 Result:").grid(column=0, row=2, padx=10, pady=10)
-    sha512_result = tk.StringVar()
-    ttk.Label(tab4, textvariable=sha512_result).grid(column=1, row=2, padx=10, pady=10)
 
     ttk.Button(tab4, text="Calculate Hashes", command=calculate_hashes).grid(column=0, row=3, columnspan=2, pady=10)
 

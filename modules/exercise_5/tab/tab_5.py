6dbfa3fa5c00c09764089bc61b184072448218f2
maximuspileus <krepekamadej@gmail.com>
2023-12-01 10:21:21 +0100

    sigend process succesfully

--- modules/exercise_5/tab/tab_5.py
+++ modules/exercise_5/tab/tab_5.py
@@ -5,6 +5,9 @@ from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 
 def create_tab5_controls(tab5):
+    # Variables to store message digest and signature
+    global message_digest, signature
+
     def generate_key_pair():
         private_key = rsa.generate_private_key(
             public_exponent=65537,
@@ -47,9 +50,17 @@ def create_tab5_controls(tab5):
         return private_key, public_key
 
     def sign_and_save_file(private_key, file_path):
+        global message_digest, signature
+
         with open(file_path, 'rb') as file:
             data = file.read()
 
+        # Compute the message digest
+        digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+        digest.update(data)
+        message_digest = digest.finalize()
+
+        # Sign the data
         signature = private_key.sign(
             data,
             padding.PSS(
@@ -58,7 +69,6 @@ def create_tab5_controls(tab5):
             ),
             hashes.SHA256()
         )
-
         # Save the data along with the signature
         signed_data = data + b'\nSIGNATURE:' + signature
         signed_file_path = file_path + ".signed"
@@ -68,15 +78,20 @@ def create_tab5_controls(tab5):
         return signed_file_path
 
     def verify_signed_file(public_key, signed_file_path):
+        # Extract the original data and signature from the signed file
         with open(signed_file_path, 'rb') as signed_file:
             signed_data = signed_file.read()
 
-        # Extract the original data and signature
-        data, _, signature = signed_data.partition(b'\nSIGNATURE:')
+        data, _, signature_from_file = signed_data.partition(b'\nSIGNATURE:')
 
+        result_text.set(result_text.get() + "\n")
+        result_text.set(result_text.get() + f"Signature: \n{signature_from_file.hex()}")
+        result_text.set(result_text.get() + "\n")
+
+        # Verify the stored signature
         try:
             public_key.verify(
-                signature,
+                signature_from_file,
                 data,
                 padding.PSS(
                     mgf=padding.MGF1(hashes.SHA256()),
@@ -93,11 +108,22 @@ def create_tab5_controls(tab5):
         _, public_key = load_keys()
         file_path = entry_file_path.get()
 
-        signed_file_path = file_path + ".signed"
-        if verify_signed_file(public_key, signed_file_path):
-            result_text.set("Signature is valid!")
+        result_text.set(result_text.get() + "\n=============VERIFICATION PROCESS=============\n")
+        result_text.set(result_text.get() + "\n")
+        # Append the computed message digest
+        result_text.set(result_text.get() + f"Computed Message Digest: {message_digest.hex()}\n")
+        result_text.set(result_text.get() + "\n")
+
+        # Verify the stored signature
+        if verify_signed_file(public_key, file_path + ".signed"):
+            result_text.set(result_text.get() + "\n")
+            result_text.set(result_text.get() +
+                            "Signature is valid!\n")
+            result_text.set(result_text.get() + "\n")
         else:
-            result_text.set("Signature is NOT valid!")
+            result_text.set(result_text.get() + "\n")
+            result_text.set(result_text.get() + "Signature is NOT valid!\n")
+            result_text.set(result_text.get() + "\n")
 
     def browse_file():
         file_path = filedialog.askopenfilename()
@@ -108,13 +134,20 @@ def create_tab5_controls(tab5):
     def generate_keys_action():
         private_key, public_key = generate_key_pair()
         save_keys(private_key, public_key)
-        result_text.set("Key pair generated and saved successfully!")
+        result_text.set("Key pair generated and saved successfully!\n")
 
     def sign_file_action():
         private_key, _ = load_keys()
         file_path = entry_file_path.get()
+
+        # Clear all text before signing
+        result_text.set("")
+
         signed_file_path = sign_and_save_file(private_key, file_path)
-        result_text.set(f"File signed successfully!\nSigned File: {signed_file_path}")
+        result_text.set(result_text.get() + "\n=============SIGNED PROCESS=============\n")
+        result_text.set(result_text.get() + f"Message Digest: {message_digest.hex()}\nSignature: {signature.hex()}\n\n"
+                        f"File signed successfully!\nSigned File: {signed_file_path}")
+        result_text.set(result_text.get() + "\n")
 
     # GUI elements
     tk.Label(tab5, text="File Path:").grid(row=0, column=0, padx=10, pady=10)

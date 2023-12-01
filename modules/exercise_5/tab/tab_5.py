d3b2deb12b462311c8ac8591d8df206867cb19c3
maximuspileus <krepekamadej@gmail.com>
2023-12-01 09:23:16 +0100

    generated rsa key pair

--- modules/exercise_5/tab/tab_5.py
+++ modules/exercise_5/tab/tab_5.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import rsa, padding, utils
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
 
 def create_tab5_controls(tab5):
     def generate_key_pair():
@@ -12,6 +12,38 @@ def create_tab5_controls(tab5):
             backend=default_backend()
         )
         public_key = private_key.public_key()
+        return private_key, public_key
+
+    def save_keys(private_key, public_key):
+        with open("private_key.pem", "wb") as private_key_file:
+            private_key_pem = private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption()
+            )
+            private_key_file.write(private_key_pem)
+
+        with open("public_key.pem", "wb") as public_key_file:
+            public_key_pem = public_key.public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo
+            )
+            public_key_file.write(public_key_pem)
+
+    def load_keys():
+        with open("private_key.pem", "rb") as private_key_file:
+            private_key = serialization.load_pem_private_key(
+                private_key_file.read(),
+                password=None,
+                backend=default_backend()
+            )
+
+        with open("public_key.pem", "rb") as public_key_file:
+            public_key = serialization.load_pem_public_key(
+                public_key_file.read(),
+                backend=default_backend()
+            )
+
         return private_key, public_key
 
     def sign_file(private_key, file_path):
@@ -27,42 +59,22 @@ def create_tab5_controls(tab5):
         )
         return signature
 
-    def verify_signature(public_key, file_path, signature):
-        with open(file_path, 'rb') as file:
-            data = file.read()
-        try:
-            public_key.verify(
-                signature,
-                data,
-                padding.PSS(
-                    mgf=padding.MGF1(hashes.SHA256()),
-                    salt_length=padding.PSS.MAX_LENGTH
-                ),
-                hashes.SHA256()
-            )
-            return True
-        except utils.InvalidSignature:
-            return False
-
     def browse_file():
         file_path = filedialog.askopenfilename()
         if file_path:
             entry_file_path.delete(0, tk.END)
             entry_file_path.insert(0, file_path)
 
+    def generate_keys_action():
+        private_key, public_key = generate_key_pair()
+        save_keys(private_key, public_key)
+        result_text.set("Key pair generated and saved successfully!")
+
     def sign_file_action():
-        private_key, _ = generate_key_pair()
+        private_key, _ = load_keys()
         file_path = entry_file_path.get()
         signature = sign_file(private_key, file_path)
         result_text.set(f"File signed successfully!\nSignature: {signature.hex()}")
-
-    def verify_signature_action():
-        _, public_key = generate_key_pair()
-        file_path = entry_file_path.get()
-        signature_hex = entry_signature.get()
-        signature = bytes.fromhex(signature_hex)
-        is_valid = verify_signature(public_key, file_path, signature)
-        result_text.set(f"Signature verification: {'Valid' if is_valid else 'Invalid'}")
 
     # GUI elements
     tk.Label(tab5, text="File Path:").grid(row=0, column=0, padx=10, pady=10)
@@ -70,14 +82,10 @@ def create_tab5_controls(tab5):
     entry_file_path.grid(row=0, column=1, padx=10, pady=10)
     tk.Button(tab5, text="Browse", command=browse_file).grid(row=0, column=2, padx=10, pady=10)
 
-    tk.Label(tab5, text="Signature:").grid(row=1, column=0, padx=10, pady=10)
-    entry_signature = tk.Entry(tab5, width=40)
-    entry_signature.grid(row=1, column=1, padx=10, pady=10)
-
+    tk.Button(tab5, text="Generate Keys", command=generate_keys_action).grid(row=1, column=0, columnspan=3, pady=10)
     tk.Button(tab5, text="Sign File", command=sign_file_action).grid(row=2, column=0, columnspan=3, pady=10)
-    tk.Button(tab5, text="Verify Signature", command=verify_signature_action).grid(row=3, column=0, columnspan=3, pady=10)
 
-    tk.Label(tab5, text="Result:").grid(row=4, column=0, padx=10, pady=10)
+    tk.Label(tab5, text="Result:").grid(row=3, column=0, padx=10, pady=10)
     result_text = tk.StringVar()
     result_label = tk.Label(tab5, textvariable=result_text, wraplength=400, justify=tk.LEFT)
-    result_label.grid(row=4, column=1, columnspan=2, padx=10, pady=10)
+    result_label.grid(row=3, column=1, columnspan=2, padx=10, pady=10)
